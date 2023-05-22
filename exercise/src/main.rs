e2b45ebfa09951c8b654c188b7a623b0360975e4
AD <silentflute.ad@gmail.com>
2023-05-22 20:48:17 +0100

    Updating exercise

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -14,4 +14,14 @@ fn main() {
         print!(" -> {x}");
     }
     println!("\n");
+
+    // Array assignment example
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a: {:?}", a);
+
+    // Tuple assignment 
+    let t: (i8, bool) = (7, true);
+    println!("1st index: {}", t.0);
+    println!("2nd index: {}", t.1);
 }

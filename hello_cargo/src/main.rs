ace05d4fb8f51b8d6ef04c70b9131e75409bcf84
AD <silentflute.ad@gmail.com>
2023-07-10 17:04:32 +0100

    Doing a hello world cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ Example using cargo 
+ - We can create a project using {cargo new}
+ - We can build a project using {cargo build}
+ - We can build and run a project in one step using {cargo run}
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
